6b92de85e43b7dabbd961867e85dc58861fca272
Dylan Beattoie <dylan@dylanbeattie.net>
2022-09-26 13:08:35 +0200

    Publish/subscribe demo

--- pubsub/Subscriber/Program.cs
+++ pubsub/Subscriber/Program.cs
@@ -3,6 +3,14 @@ using Messages;
 
 const string AMQP = "amqps://aqbgvtbw:DLZt9nFoITcQrb-ddTm6duOOs7EHnwF3@large-yellow-chameleon.rmq4.cloudamqp.com/aqbgvtbw";
 var bus = RabbitHutch.CreateBus(AMQP);
-bus.PubSub.Subscribe<Greeting>("SUBSCRIPTION_ID", Console.WriteLine);
+bus.PubSub.Subscribe<Greeting>("dylan_beattie", Handle);
 Console.WriteLine("Listening for messages...");
 Console.ReadKey(false);
+
+
+void Handle(Greeting greeting) {
+    if (greeting.Number % 5 == 0) {
+        throw new Exception("Something weird happened!");
+    }
+    Console.WriteLine(greeting);
+}
