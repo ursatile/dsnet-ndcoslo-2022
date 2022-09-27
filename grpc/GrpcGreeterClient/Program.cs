a7806eceae7e5fabaab2c9b1bf8c625cb0088f1d
Dylan Beattoie <dylan@dylanbeattie.net>
2022-09-27 11:21:29 +0200

    gRPC working before we broke stuff

---
+++ grpc/GrpcGreeterClient/Program.cs
@@ -0,0 +1,16 @@
+﻿using GrpcGreeter;
+using Grpc.Net.Client;
+using System.Threading.Tasks;
+
+using var channel = GrpcChannel.ForAddress("https://localhost:7043");
+var client = new Greeter.GreeterClient(channel);
+while(true) {
+    Console.WriteLine("Press any key to send a gRPC request:");
+    Console.ReadKey(true);
+    var reply = client.SayHello(new HelloRequest {
+        Name = "NDC Oslo"
+    });
+    Console.WriteLine(reply.Message);
+}
+
+
