a3b9335cd22d32b7944c24d022873b27c4dd15e8
Dylan Beattoie <dylan@dylanbeattie.net>
2022-09-27 11:31:57 +0200

    gRPC working with Langhuage field

--- grpc/GrpcGreeter/Services/GreeterService.cs
+++ grpc/GrpcGreeter/Services/GreeterService.cs
@@ -3,19 +3,22 @@ using GrpcGreeter;
 
 namespace GrpcGreeter.Services;
 
-public class GreeterService : Greeter.GreeterBase
-{
+public class GreeterService : Greeter.GreeterBase {
     private readonly ILogger<GreeterService> _logger;
-    public GreeterService(ILogger<GreeterService> logger)
-    {
+    public GreeterService(ILogger<GreeterService> logger) {
         _logger = logger;
     }
 
-    public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
-    {
-        return Task.FromResult(new HelloReply
-        {
-            Message = "Hello " + request.Name
+    public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context) {
+        var message = request.Language switch {
+            "en-GB" => $"Good morning, {request.Name}!",
+            "no-NB" => $"God morgen, {request.Name}",
+            "en-AU" => $"G'day, {request.Name}",
+            _ => $"Hello {request.Name}"
+        };
+
+        return Task.FromResult(new HelloReply {
+            Message = message
         });
     }
 }
--- grpc/GrpcGreeterClient/Program.cs
+++ grpc/GrpcGreeterClient/Program.cs
@@ -1,4 +1,4 @@
-﻿using GrpcGreeter;
+using GrpcGreeter;
 using Grpc.Net.Client;
 using System.Threading.Tasks;
 
@@ -6,8 +6,14 @@ using var channel = GrpcChannel.ForAddress("https://localhost:7043");
 var client = new Greeter.GreeterClient(channel);
 while(true) {
     Console.WriteLine("Press any key to send a gRPC request:");
-    Console.ReadKey(true);
+    var language = Console.ReadKey(true).KeyChar switch {
+        '1' => "en-GB",
+        '2' => "no-NB",
+        '3' => "en-AU",
+        _ => String.Empty
+    };
     var reply = client.SayHello(new HelloRequest {
+        Language = language,
         Name = "NDC Oslo"
     });
     Console.WriteLine(reply.Message);
