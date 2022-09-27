44fe48dfff3958d2a4e2cb37263e82ab7c7e7218
Dylan Beattoie <dylan@dylanbeattie.net>
2022-09-27 10:06:27 +0200

    GraphQL including query by colour

--- dotnet/Autobarn.Website/GraphQL/Queries/VehicleQuery.cs
+++ dotnet/Autobarn.Website/GraphQL/Queries/VehicleQuery.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Autobarn.Data;
 using Autobarn.Data.Entities;
 using Autobarn.Website.GraphQL.GraphTypes;
@@ -14,13 +16,43 @@ namespace Autobarn.Website.GraphQL.Queries {
         public VehicleQuery(IAutobarnDatabase db) {
             this.db = db;
 
-            Field<ListGraphType<VehicleGraphType>>("Vehicles")
+            Field<ListGraphType<VehicleGraphType>>("vehicles")
                 .Description("Return all vehicles")
                 .Resolve(GetAllVehicles);
+
+            Field<VehicleGraphType>("vehicle")
+                .Description("Get a single vehicle")
+                .Arguments(MakeNonNullStringArgument("registration", "The registration of the vehicle you want"))
+                .Resolve(GetVehicle);
+
+
+            Field<ListGraphType<VehicleGraphType>>("vehiclesByColor")
+                .Description("Get all vehicles matching a certain colour")
+                .Arguments(MakeNonNullStringArgument("color", "What color cars do you want?"))
+                .Resolve(GetVehiclesByColor);
+
+        }
+
+        private object GetVehiclesByColor(IResolveFieldContext<object> context) {
+            var color = context.GetArgument<string>("color");
+            return db.ListVehicles().Where(v => v.Color.Contains(color, StringComparison.InvariantCultureIgnoreCase));
+        }
+
+        private QueryArgument MakeNonNullStringArgument(string name, string description) {
+            return new QueryArgument<NonNullGraphType<StringGraphType>> {
+                Name = name, Description = description
+            };
+        }
+
+        private Vehicle GetVehicle(IResolveFieldContext<object> context) {
+            var reg = context.GetArgument<string>("registration");
+            return db.FindVehicle(reg);
         }
 
         private IEnumerable<Vehicle> GetAllVehicles(IResolveFieldContext<object> arg) {
             return db.ListVehicles();
         }
+
+
     }
 }
