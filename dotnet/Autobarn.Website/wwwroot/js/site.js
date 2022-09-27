1dd1601ede5b2a2ce46f4f5805669c8d7f18acdf
Dylan Beattoie <dylan@dylanbeattie.net>
2022-09-27 15:09:22 +0200

    SignalR working with browser console.log

--- dotnet/Autobarn.Website/wwwroot/js/site.js
+++ dotnet/Autobarn.Website/wwwroot/js/site.js
@@ -1,4 +1,19 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).ready(function () {
+    const conn = new signalR.HubConnectionBuilder().withUrl("/hub").build();
+    conn.on("LookAnotherAmazingMagicString",
+        function (user, message) {
+            console.log(user);
+            console.log(message);
+        });
+    conn.start().then(function () {
+        console.log("Connected to SignalR!");
+    }).catch(function (err) {
+        console.log("Failed!");
+        console.log(err);
+    });
+});
